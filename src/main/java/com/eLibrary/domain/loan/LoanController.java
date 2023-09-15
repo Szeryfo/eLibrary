4c733be746c659901ab49addcd9168e711e024ac
Sebastian Kurzel <Sebastian.Kurzel@ttsw.com.pl>
2023-09-15 11:23:50 +0200

    fixed swagger configuration and added some methods in loan controller

--- src/main/java/com/eLibrary/domain/loan/LoanController.java
+++ src/main/java/com/eLibrary/domain/loan/LoanController.java
@@ -1,5 +1,6 @@
 package com.eLibrary.domain.loan;
 
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,16 +18,37 @@ class LoanController {
     @Autowired
     LoanFacade facade;
 
+    @Operation(summary = "Get all loans")
     @GetMapping
     public List<LoanDto> getAll() {
         return facade.getAll();
     }
 
+    @Operation(summary = "Get loans by given user id")
+    @GetMapping("/user/{userId}")
+    public List<LoanDto> getByUserId(@PathVariable Long userId) {
+        return null;
+    }
+
+    @Operation(summary = "Get loans by given book id")
+    @GetMapping("/book/{bookId}")
+    public List<LoanDto> getByBookId(@PathVariable Long bookId) {
+        return null;
+    }
+
+    @Operation(summary = "Get late loans")
+    @GetMapping("/late")
+    public List<LoanDto> getLateLoans() {
+        return null;
+    }
+
+    @Operation(summary = "Save loan")
     @PostMapping
     public LoanDto save(@RequestBody LoanDto userDto) {
         return facade.save(userDto);
     }
 
+    @Operation(summary = "Delete loan by given id")
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Long id) {
         facade.deleteById(id);
