b0a51e7fcbbfc7fe95a4abdbb90c979210dbe75d
Sebastian Kurzel <sebastian.kurzel@ttsw.com.pl>
2024-02-08 16:55:28 +0100

    test 2

--- src/main/java/com/eLibrary/Application.java
+++ src/main/java/com/eLibrary/Application.java
@@ -9,7 +9,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-        // for td
+        // for tdsdsa
         System.exit(0);
     }
 
